c2ff5db055c6920e6bdda1d8a117ccd9e3e5c752
Jakub Madejczyk <jakub.madejczyk@avanade.com>
2024-04-05 23:40:07 +0200

    wrap header

--- app/common/Header.tsx
+++ app/common/Header.tsx
@@ -3,7 +3,7 @@ import {Narzedzia} from './Narzedzia'
 
 export const Header = () => {
     return <div className='header'>
-        <div style={{display: 'flex', alignItems: 'center'}}>
+        <div style={{display: 'flex', alignItems: 'center', flexWrap: 'wrap'}}>
             <img
                 src="/logo.png"
                 width={250}
@@ -13,49 +13,51 @@ export const Header = () => {
             <h1>PPP 1<br/>GORZÓW</h1>
         </div>
         <div>
-            <div style={{display: 'flex', justifyContent:'flex-end', gap: '1rem', alignItems: 'center'}}>
+            <div style={{display: 'flex', justifyContent:'flex-end', gap: '1rem', alignItems: 'center', flexWrap: 'wrap'}}>
                 <Narzedzia/>
-                <Link
-                    title="https://www.powiatgorzowski.pl/ - Otwiera się w nowym oknie przeglądarki"
-                    href='https://www.powiatgorzowski.pl/'
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src="/powiat-gorzowski.png"
-                        width={50}
-                        height={47}
-                        alt="Logo - powiatu gorzowskiego"
-                    />
-                </Link>
-                <Link
-                    title="https://bip.wrota.lubuskie.pl/ppp1 - Otwiera się w nowym oknie przeglądarki"
-                    href='https://bip.wrota.lubuskie.pl/ppp1'
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        style={{backgroundColor: 'white'}}
-                        src="/bip.svg"
-                        width={50}
-                        height={47}
-                        alt="Logo - biuletyn informacji publicznej"
-                    />
-                </Link>
-                <Link
-                    title="https://www.facebook.com/poradnianr1gorzow - Otwiera się w nowym oknie przeglądarki"
-                    href='https://www.facebook.com/poradnianr1gorzow'
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        src="/facebook.png"
-                        width={50}
-                        height={47}
-                        alt="Logo - facebook"
-                    />
-                </Link>
+                <div style={{display: 'flex', justifyContent: 'center', gap: '1rem'}}>
+                    <Link
+                        title="https://www.powiatgorzowski.pl/ - Otwiera się w nowym oknie przeglądarki"
+                        href='https://www.powiatgorzowski.pl/'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src="/powiat-gorzowski.png"
+                            width={50}
+                            height={47}
+                            alt="Logo - powiatu gorzowskiego"
+                        />
+                    </Link>
+                    <Link
+                        title="https://bip.wrota.lubuskie.pl/ppp1 - Otwiera się w nowym oknie przeglądarki"
+                        href='https://bip.wrota.lubuskie.pl/ppp1'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            style={{backgroundColor: 'white'}}
+                            src="/bip.svg"
+                            width={50}
+                            height={47}
+                            alt="Logo - biuletyn informacji publicznej"
+                        />
+                    </Link>
+                    <Link
+                        title="https://www.facebook.com/poradnianr1gorzow - Otwiera się w nowym oknie przeglądarki"
+                        href='https://www.facebook.com/poradnianr1gorzow'
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img
+                            src="/facebook.png"
+                            width={50}
+                            height={47}
+                            alt="Logo - facebook"
+                        />
+                    </Link>
                 </div>
+            </div>
             <nav style={{textTransform: 'uppercase'}}>
                 <Link href="/#">Strona Główna</Link>
                 <Link href="/aktualnosci">Aktualności</Link>
