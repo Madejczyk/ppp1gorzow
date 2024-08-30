5ce2b5dce76f030984a29a9bcc1bf6b1f2ca2843
Jakub Madejczyk <jakub.madejczyk@avanade.com>
2024-08-30 09:43:26 +0200

    nowa oferta

--- app/oferta/page.tsx
+++ app/oferta/page.tsx
@@ -7,6 +7,12 @@ export default function Oferta() {
     return <main>
         <h2>{title}</h2>
         <Link
+            href="/pliki/Oferta_PPP1_2024-2025.doc"
+            target="_blank"
+            rel="noopener noreferrer"
+            title='Link otwiera się w nowym oknie przeglądarki'
+        >Oferta PPP1 2024-2025 (51 KB, DOC)</Link>
+        <Link
             href="/pliki/Oferta_PPP1_2023-2024.pdf"
             target="_blank"
             rel="noopener noreferrer"
