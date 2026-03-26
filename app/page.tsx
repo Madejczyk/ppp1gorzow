d72a0cd97361ccbc3e15dd24a218faa3e82406a0
Jakub Madejczyk <Madejczyk@users.noreply.github.com>
2026-03-26 21:22:33 +0100

    Add YouTube iframe for video embedding

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,9 @@ export default function Home() {
         </ul>
 
         <Informacja/>
+        <iframe width="420" height="315"       
+src="https://www.youtube.com/embed/Hv2d0xkTEOo?autoplay=1">
+</iframe>
         <Map/>
     </main>
 }
